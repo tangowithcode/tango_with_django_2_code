b90fc0b52abda784e09fd8ee99c9f7d356e54470
David Maxwell <maxwelld90@gmail.com>
2020-01-11 16:20:43 +0000

    Completed exercises for Chapter 7.

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
     path('add_category/', views.add_category, name='add_category'),
+    path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
 ]
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,6 +4,8 @@ from rango.models import Category
 from rango.models import Page
 from rango.forms import CategoryForm
 from django.shortcuts import redirect
+from django.urls import reverse
+from rango.forms import PageForm
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -49,3 +51,32 @@ def add_category(request):
             print(form.errors)
     
     return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except:
+        category = None
+    
+    # You cannot add a page to a Category that does not exist... DM
+    if category is None:
+        return redirect('/rango/')
+
+    form = PageForm()
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+
+                return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
+        else:
+            print(form.errors)  # This could be better done; for the purposes of TwD, this is fine. DM.
+    
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'rango/add_page.html', context=context_dict)
