ee8e647e327f096bf0d6b607b19e1c99fa101e22
David Maxwell <maxwelld90@gmail.com>
2020-01-11 16:58:01 +0000

    Completed everything up to the end of Chapter 8. Tests all pass.

--- tango_with_django_project/rango/templatetags/rango_template_tags.py
+++ tango_with_django_project/rango/templatetags/rango_template_tags.py
@@ -4,5 +4,6 @@ from rango.models import Category
 register = template.Library()
 
 @register.inclusion_tag('rango/categories.html')
-def get_category_list():
-    return {'categories': Category.objects.all()}
+def get_category_list(current_category=None):
+    return {'categories': Category.objects.all(),
+            'current_category': current_category}
