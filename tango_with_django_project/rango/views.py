a05319c0a35c8a11db8ea2731786e00b37fc6ed5
David Maxwell <maxwelld90@gmail.com>
2020-01-11 17:53:52 +0000

    Added cookie session visits counter example

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from datetime import datetime
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -15,17 +16,14 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
     context_dict['extra'] = 'From the model solution on GitHub'
+    context_dict['visits'] = int(request.COOKIES.get('visits', '1'))
 
-    # Cookie tasting
-    request.session.set_test_cookie()
+    response = render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request, response)
 
-    return render(request, 'rango/index.html', context=context_dict)
+    return response
 
 def about(request):
-    if request.session.test_cookie_worked():
-        print("TEST COOKIE WORKED!")
-        request.session.delete_test_cookie()
-
     # Spoiler: you don't need to pass a context dictionary here.
     return render(request, 'rango/about.html')
 
@@ -144,3 +142,16 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        response.set_cookie('last_visit', last_visit_cookie)
+    
+    response.set_cookie('visits', visits)
