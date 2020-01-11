7da3c375c429b4e23f4ac3488376f9bfa30107ea
David Maxwell <maxwelld90@gmail.com>
2020-01-11 21:29:16 +0000

    Removed decommissioned search() view

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -118,19 +118,7 @@ def visitor_cookie_handler(request):
         request.session['last_visit'] = last_visit_cookie
     
     request.session['visits'] = visits
-
-# def search(request):
-#     result_list = []
-#     query = ''
-
-#     if request.method == 'POST':
-#         query = request.POST['query'].strip()
-
-#         if query:
-#             result_list = run_query(query)
     
-#     return render(request, 'rango/search.html', {'result_list': result_list, 'query': query})
-
 def goto_url(request):
     if request.method == 'GET':
         page_id = request.GET.get('page_id')
