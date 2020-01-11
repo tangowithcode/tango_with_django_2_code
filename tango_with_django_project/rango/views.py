0182ccc31957c1f1c08c210f19380dacd4b9ea41
David Maxwell <maxwelld90@gmail.com>
2020-01-11 17:42:48 +0000

    Chapter 9 exercises now complete.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -37,6 +37,7 @@ def show_category(request, category_name_slug):
     
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -51,6 +52,7 @@ def add_category(request):
     
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -129,7 +131,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
 
 @login_required
 def user_logout(request):
