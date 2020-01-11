04a4592cdf59f4cd8fee0d729ac522bff51cec05
David Maxwell <maxwelld90@gmail.com>
2020-01-11 13:41:33 +0000

    Category slugs taken care of

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -4,5 +4,8 @@ from rango.models import Category, Page
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Categories'
