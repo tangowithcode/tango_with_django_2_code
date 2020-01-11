fce330ddab5f22529f214adc0a456ee1d362b32d
David Maxwell <maxwelld90@gmail.com>
2020-01-11 14:02:11 +0000

    Added a new category view, and added link from index page to the said new view

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -6,4 +6,5 @@ app_name = 'rango'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
 ]
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -15,3 +16,18 @@ def index(request):
 def about(request):
     # Spoiler: you don't need to pass a context dictionary here.
     return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None
+    
+    return render(request, 'rango/category.html', context=context_dict)
