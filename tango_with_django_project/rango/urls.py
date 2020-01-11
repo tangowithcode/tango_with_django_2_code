bac096b7ab2ab7e82d21c24cade464609499676e
David Maxwell <maxwelld90@gmail.com>
2020-01-11 12:31:08 +0000

    Initial view working, along with rango app URL mappings.

---
+++ tango_with_django_project/rango/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rango import views
+
+app_name = 'rango'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from rango import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('rango/', include('rango.urls')),
+    # 3 - The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
 ]
