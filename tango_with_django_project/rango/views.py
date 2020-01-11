22669b3a44e748a30429916cfc6c00b450425926
David Maxwell <maxwelld90@gmail.com>
2020-01-11 17:58:58 +0000

    Now using session data.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -16,11 +16,11 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
     context_dict['extra'] = 'From the model solution on GitHub'
-    context_dict['visits'] = int(request.COOKIES.get('visits', '1'))
+    
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
 
     response = render(request, 'rango/index.html', context=context_dict)
-    visitor_cookie_handler(request, response)
-
     return response
 
 def about(request):
@@ -143,15 +143,21 @@ def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
 
-def visitor_cookie_handler(request, response):
-    visits = int(request.COOKIES.get('visits', '1'))
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
 
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
 
     if (datetime.now() - last_visit_time).days > 0:
-        response.set_cookie('last_visit', str(datetime.now()))
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
     else:
-        response.set_cookie('last_visit', last_visit_cookie)
+        request.session['last_visit'] = last_visit_cookie
     
-    response.set_cookie('visits', visits)
+    request.session['visits'] = visits
