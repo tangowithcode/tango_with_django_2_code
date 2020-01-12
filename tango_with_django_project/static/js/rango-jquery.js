97964cf5d69edd0ac1b92ebc5271ac218862ae2f
David Maxwell <maxwelld90@gmail.com>
2020-01-12 12:20:32 +0000

    Added element manipulation JQuery example.

--- tango_with_django_project/static/js/rango-jquery.js
+++ tango_with_django_project/static/js/rango-jquery.js
@@ -11,5 +11,12 @@ $(document).ready(function() {
         function() {
             $(this).css('color', 'black');
         });
+    
+    $('#about-btn').click(function() {
+        msgStr = $('#msg').html();
+        msgStr = msgStr + " ooo, fancy!";
+
+        $('#msg').html(msgStr);
+    })
 
 })
