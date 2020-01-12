93de8310e8135972cbca5c1723c6defb8c785776
David Maxwell <maxwelld90@gmail.com>
2020-01-12 14:36:21 +0000

    Second test added, for checking slug values

--- tango_with_django_project/rango/tests.py
+++ tango_with_django_project/rango/tests.py
@@ -10,3 +10,13 @@ class CategoryMethodTests(TestCase):
         category.save()
 
         self.assertEqual((category.views >=0), True)
+    
+    def test_slug_line_creation(self):
+        """
+        Checks to make sure that when a category is created, an appropriate slug is created.
+        Example: "Random Category String" should be "random-category-string".
+        """
+        category = Category(name='Random Category String')
+        category.save()
+
+        self.assertEqual(category.slug, 'random-category-string')
