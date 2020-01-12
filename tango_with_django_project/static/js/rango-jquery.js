4853f1de7c2589c999c5d1ea64775b37130749e5
David Maxwell <maxwelld90@gmail.com>
2020-01-12 12:16:37 +0000

    Added red hover text for paragraph

--- tango_with_django_project/static/js/rango-jquery.js
+++ tango_with_django_project/static/js/rango-jquery.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
 
     $('#about-btn').click(function() {
         alert("You clicked the button using JQuery!");
-    })
+    });
+
+    $('p').hover(
+        function() {
+            $(this).css('color', 'red');
+        },
+        function() {
+            $(this).css('color', 'black');
+        });
 
 })
