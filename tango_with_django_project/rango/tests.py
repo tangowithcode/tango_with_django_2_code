9d8c5bfc8b07be8a02282c037fc897d43addf383
David Maxwell <maxwelld90@gmail.com>
2020-01-12 14:43:39 +0000

    Added test helper function and IndexViewTests.

--- tango_with_django_project/rango/tests.py
+++ tango_with_django_project/rango/tests.py
@@ -1,5 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 from rango.models import Category
+
+def add_category(name, views=0, likes=0):
+    category = Category.objects.get_or_create(name=name)[0]
+    category.views = views
+    category.likes = likes
+
+    category.save()
+    return category
 
 class CategoryMethodTests(TestCase):
     def test_ensure_views_are_positive(self):
@@ -20,3 +29,31 @@ class CategoryMethodTests(TestCase):
         category.save()
 
         self.assertEqual(category.slug, 'random-category-string')
+
+class IndexViewTests(TestCase):
+    def test_index_view_with_no_categories(self):
+        """
+        If no categories exist, the appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('rango:index'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'There are no categories present.')
+        self.assertQuerysetEqual(response.context['categories'], [])
+    
+    def test_index_view_with_categories(self):
+        """
+        Checks whether categories are displayed correctly when present.
+        """
+        add_category('Python', 1, 1)
+        add_category('C++', 1, 1)
+        add_category('Erlang', 1, 1)
+
+        response = self.client.get(reverse('rango:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Python')
+        self.assertContains(response, 'C++')
+        self.assertContains(response, 'Erlang')
+
+        num_categories = len(response.context['categories'])
+        self.assertEquals(num_categories, 3)
